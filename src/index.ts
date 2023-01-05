e940a22b460f8a7036f9bc86f74068186a271a9f
Terry Fei <i@terryfei.com>
2023-01-05 18:01:01 +0800

    init

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import "./index.css";
+
+import type {
+  HyperFunctionComponent,
+  HfcProps,
+} from "hyper-function-component";
+
+const HFC: HyperFunctionComponent<HTMLButtonElement> = (container, initProps) => {
+  container.innerHTML = `
+      <h1>
+        <div>THIS COMPONENT</div>
+        <div>CAN BE USED IN</div>
+        <div class="brand">
+          <ul>
+            <li>React</li>
+            <li>Vue</li>
+            <li>Hfz</li>
+            <li>Angular</li>
+            <li>WebComponents</li>
+          </ul>
+        </div>
+      </h1>`;
+
+  return { changed(props: HfcProps) {}, disconnected() {} };
+};
+
+HFC.tag = "button";
+// @ts-ignore
+HFC.hfc = process.env.HFC_NAME;
+// @ts-ignore
+HFC.ver = process.env.HFC_VERSION;
+// @ts-ignore
+HFC.names = process.env.HFC_PROP_NAMES;
+
+export default HFC;
